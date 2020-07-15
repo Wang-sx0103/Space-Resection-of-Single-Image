ec73c86fe56065bc1732d34808fc4d8868250714
王尚兴 <52378569+Wang-sx0103@users.noreply.github.com>
2020-07-15 12:58:47 +0800

    添加y轴的伸缩量与平移量的结果

--- CT.py
+++ CT.py
@@ -13,47 +13,79 @@ xF = []                                                                         
 yF = []
 aTranslation = []                                                               #坐标变换中的平移量
 bFlexible = []                                                                  #坐标变换中的伸缩量，不难看出x、y轴有相同的伸缩量和平移量
+
 dx = 1
 dy = 1
-x0 =  114.0
-y0 =  22.0
+xx0 =  114.0
+xy0 =  22.0
 
-while abs(dx)>0.00001 and abs(dy)>0.00001:
+while abs(dx)>0.000001 and abs(dy)>0.000001:
     x = np.mat(np.zeros((2,1)))
-    T0 = xFrame[0]-x0
-    T1 = xFrame[1]-x0
-    T2 = xFrame[2]-x0
-    T3 = xFrame[3]-x0
-    T4 = xFrame[4]-x0
-    T5 = xFrame[5]-x0
-    T6 = xFrame[6]-x0
-    T7 = xFrame[7]-x0
-    A = np.mat([[y0,T0],[y0,T1],[y0,T2],[y0,T3],[y0,T4],[y0,T5],[y0,T6],[y0,T7]])
-    aMatrix = np.mat(A)
-    
-    T0 = xPixel[0]-x0*y0-y0*xFrame[0]
-    T1 = xPixel[1]-x0*y0-y0*xFrame[1]
-    T2 = xPixel[2]-x0*y0-y0*xFrame[2]
-    T3 = xPixel[3]-x0*y0-y0*xFrame[3]
-    T4 = xPixel[4]-x0*y0-y0*xFrame[4]
-    T5 = xPixel[5]-x0*y0-y0*xFrame[5]
-    T6 = xPixel[6]-x0*y0-y0*xFrame[6]
-    T7 = xPixel[7]-x0*y0-y0*xFrame[7]
-    B = np.mat([T0,T1,T2,T3,T4,T5,T6,T7])#,dtype=np.float)
-    bMatrix = np.mat(B)
+    T0 = xFrame[0]-xx0
+    T1 = xFrame[1]-xx0
+    T2 = xFrame[2]-xx0
+    T3 = xFrame[3]-xx0
+    T4 = xFrame[4]-xx0
+    T5 = xFrame[5]-xx0
+    T6 = xFrame[6]-xx0
+    T7 = xFrame[7]-xx0
+    A = np.mat([[xy0,T0],[xy0,T1],[xy0,T2],[xy0,T3],[xy0,T4],[xy0,T5],[xy0,T6],[xy0,T7]])
+    T0 = xPixel[0]-xx0*xy0-xy0*xFrame[0]
+    T1 = xPixel[1]-xx0*xy0-xy0*xFrame[1]
+    T2 = xPixel[2]-xx0*xy0-xy0*xFrame[2]
+    T3 = xPixel[3]-xx0*xy0-xy0*xFrame[3]
+    T4 = xPixel[4]-xx0*xy0-xy0*xFrame[4]
+    T5 = xPixel[5]-xx0*xy0-xy0*xFrame[5]
+    T6 = xPixel[6]-xx0*xy0-xy0*xFrame[6]
+    T7 = xPixel[7]-xx0*xy0-xy0*xFrame[7]
+    B = np.mat([T0,T1,T2,T3,T4,T5,T6,T7])
     x = (A.T*A).I*A.T*B.T
-    dx = x.tolist()[0][0]
-    dy = x.tolist()[1][0]
-    x0 = x0+dx
-    y0 = y0+dy
+    dx = x[0,0]
+    dy = x[1,0]
+    xx0 = xx0+dx
+    xy0 = xy0+dy
 
-print(x0)
-print(y0)
+print("x轴的平移量:",xx0)
+print("x轴的平移量:",xy0)
+
+dx = 1
+dy = 1
+yx0 =  114.0
+yy0 =  22.0
+
+while abs(dx)>0.000001 and abs(dy)>0.000001:
+    x = np.mat(np.zeros((2,1)))
+    T0 = yFrame[0]-yx0
+    T1 = yFrame[1]-yx0
+    T2 = yFrame[2]-yx0
+    T3 = yFrame[3]-yx0
+    T4 = yFrame[4]-yx0
+    T5 = yFrame[5]-yx0
+    T6 = yFrame[6]-yx0
+    T7 = yFrame[7]-yx0
+    A = np.mat([[yy0,T0],[yy0,T1],[yy0,T2],[yy0,T3],[yy0,T4],[yy0,T5],[yy0,T6],[yy0,T7]])
+    T0 = yPixel[0]-yx0*yy0-yy0*yFrame[0]
+    T1 = yPixel[1]-yx0*yy0-yy0*yFrame[1]
+    T2 = yPixel[2]-yx0*yy0-yy0*yFrame[2]
+    T3 = yPixel[3]-yx0*yy0-yy0*yFrame[3]
+    T4 = yPixel[4]-yx0*yy0-yy0*yFrame[4]
+    T5 = yPixel[5]-yx0*yy0-yy0*yFrame[5]
+    T6 = yPixel[6]-yx0*yy0-yy0*yFrame[6]
+    T7 = yPixel[7]-yx0*yy0-yy0*yFrame[7]
+    B = np.mat([T0,T1,T2,T3,T4,T5,T6,T7])
+    x = (A.T*A).I*A.T*B.T
+    dx = x[0,0]
+    dy = x[1,0]
+    yx0 = yx0 + dx
+    yy0 = yy0 + dy
+
+print("y轴的平移量:",yx0)
+print("y轴的伸缩量:",yy0)
 
 for j in range(len(xControl)):
-    x = xControl[j]/y0 - x0
-    y = yControl[j]/y0 - x0
+    x = xControl[j]/xy0 - xx0
+    y = yControl[j]/yy0 - yx0
     xF.append(round(x,3))
     yF.append(round(y,3))
-print(xF)
-print(yF)
+print("框标平面坐标下的x：",xF)
+print("框标平面坐标下的y：",yF)
